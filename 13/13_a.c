e8c0de4526ca90c86e0f23a08b1462504dd8c531
Andre <andrehcmoraes@gmail.com>
2021-12-13 11:01:11 -0300

    Updated my calendar.

---
+++ 13/13_a.c
@@ -0,0 +1,140 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Generic stack
+typedef struct stack {
+  void *v;
+  struct stack *next;
+} stack_t;
+
+void stack_append(stack_t **s, void *v) {
+  stack_t *t = (stack_t *)malloc(sizeof(*t));
+  t->next = *s;
+  t->v = v;
+  *s = t;
+}
+
+void stack_free(stack_t **s) {
+  while(*s != NULL) {
+    stack_t *t = *s;
+    *s = (*s)->next;
+    if(t->v != NULL) free(t->v);
+    free(t);
+  }
+}
+
+typedef struct point {
+  int x,y;
+} point_t;
+
+void points_append(stack_t **s, int x, int y) {
+  point_t *p = (point_t *)malloc(sizeof(*p));
+  p->x = x;
+  p->y = y;
+  stack_append(s, (void *)p);
+}
+
+unsigned long long grid_fold(char **grid, int *lx, int *ly, char c, int n) {
+  unsigned long long visible = 0;
+  int len_x = *lx;
+  int len_y = *ly;
+  int nx, ny, dx, dy, z;
+  dx = dy = 0;
+  nx = ny = 0;
+
+  if(c=='y') {
+    // Fold vertically
+    ny = 1+2*n;
+    len_y = n;
+    dy = 1;
+  } else if(c=='x') {
+    // Fold horizontally
+    nx = 1+2*n;
+    len_x = n;
+    dx = 1;
+  } else {
+    printf("[!] Invalid fold %c\n", c);
+    exit(-1);
+  }
+
+  // Fold
+  z = dx ? nx : 0;
+  for(int y=0; y<len_y; y++) {
+    ny = dy ? ny - 1 : y;
+    for(int x=0; x<len_x; x++) {
+      nx = dx ? nx - 1 : x;
+      if(grid[ny][nx] == '#') grid[y][x] = grid[ny][nx];
+    }
+    nx = dx ? z : 0;
+  }
+  
+  
+  // Count visible
+  for(int y=0; y<len_y; y++) {
+    for(int x=0; x<len_x; x++) {
+        if(grid[y][x] == '#') visible++;
+    }
+  }
+  
+  *lx = len_x;
+  *ly = len_y;
+  return visible;
+}
+
+int main() {
+  // Stack points
+  stack_t *points = NULL;
+  int x,y;
+  int len_x, len_y;
+  len_x = len_y = 0;
+  while(scanf(" %d,%d", &x, &y) > 0) {
+    len_x = len_x < x ? x : len_x;
+    len_y = len_y < y ? y : len_y;
+    points_append(&points, x, y);
+  }
+  // Actual lengths are not the same as largest point
+  len_y*=2;
+  len_x*=2;
+
+  // Build grid
+  char **grid;
+  grid = (char **)malloc(len_y * sizeof(*grid));
+  for(int i=0; i<len_y; i++) {
+    grid[i] = (char *)malloc(len_x * sizeof(*(grid[i])));
+    for(int j=0; j<len_x; j++)
+      grid[i][j] = '.';
+  }
+
+
+  // Apply points
+  stack_t *t = points;
+  while(t != NULL) {
+    point_t *p = (point_t *)(t->v);
+    t = t->next;
+    grid[p->y][p->x] = '#';
+  }
+  stack_free(&points);
+  
+  // Do folds
+  int n;
+  char c;
+  int lx, ly;
+
+  lx = len_x;
+  ly = len_y;
+  unsigned long long res;
+  while(scanf(" fold along %c=%d", &c, &n) > 0) {
+    res = grid_fold(grid, &lx, &ly, c, n);
+    
+    // First fold only
+    break;
+  }
+
+  printf("Visible points:%llu\n", res);
+
+  for(int i=0; i<len_y; i++)
+    free(grid[i]);
+  free(grid);
+  
+  return 0;
+}
